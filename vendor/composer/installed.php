b0f7bcbc7c1e4d087470da857ac07d16479357c1
Alex Standiford <a@alexstandiford.com>
2022-08-08 06:15:01 -0400

    WIP

---
+++ vendor/composer/installed.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f8ac58bbb3c699d8a0175d1a7185fbd2d2133db5',
+    'name' => 'vendor_name/plugin-name-replace-me',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af93ba6e52236418f07a278033eba6959ee5b983',
+    ),
+    'laravel/serializable-closure' => 
+    array (
+      'pretty_version' => 'v1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '09f0e9fb61829f628205b7c94906c28740ff9540',
+    ),
+    'php-di/invoker' => 
+    array (
+      'pretty_version' => '2.3.3',
+      'version' => '2.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cd6d9f267d1a3474bdddf1be1da079f01b942786',
+    ),
+    'php-di/php-di' => 
+    array (
+      'pretty_version' => '6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae0f1b3b03d8b29dff81747063cbfd6276246cc4',
+    ),
+    'php-di/phpdoc-reader' => 
+    array (
+      'pretty_version' => '2.2.1',
+      'version' => '2.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '66daff34cbd2627740ffec9469ffbac9f8c8185c',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '513e0666f7216c7459170d56df27dfcefe1689ea',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '^1.0',
+      ),
+    ),
+    'underpin/underpin' => 
+    array (
+      'pretty_version' => 'dev-release/3.0.0',
+      'version' => 'dev-release/3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '48e40467335db854ba0cc1442c7a021ab73d482b',
+    ),
+    'underpin/wordpress-integration' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '4de98ce0b640abf9ea70c8031db1d2c939a7f095',
+    ),
+    'vendor_name/plugin-name-replace-me' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f8ac58bbb3c699d8a0175d1a7185fbd2d2133db5',
+    ),
+  ),
+);
